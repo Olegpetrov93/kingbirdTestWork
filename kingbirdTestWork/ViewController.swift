984bb961c9f9c1ee2cb3edeaf05298321cc5afe1
olegpetrov93 <sanderos2008@yandex.ru>
2021-07-07 20:43:56 +0300

    Initial Commit

---
+++ kingbirdTestWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kingbirdTestWork
+//
+//  Created by Oleg on 7/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
